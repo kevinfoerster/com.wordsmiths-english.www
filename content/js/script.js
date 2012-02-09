faa02367b38c948cdda71340f74104f5e496482e
Kevin Foerster <kevin.foerster@gmail.com>
2012-02-09 01:32:37 +0100

    added vertical tabs

--- content/js/script.js
+++ content/js/script.js
@@ -1,26 +1,25 @@
 /* Author: 
 
 */
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+$(document).ready(function(){
+  // $('.writing_services>dl').tabs();
+  // $('.additional_services>dl').tabs();
+  
+  $('dl').addClass('clearfix')
+  $('dd').addClass('hidden')
+  
+  $.each($('dl'), function(key, element) {
+    $(element).find('dd').first().removeClass('hidden');
+    $(element).find('dt').click(function(event) {
+      index = $.inArray($(event.target)[0], $(event.target).siblings('dt').andSelf())
+        tabContents = $($(event.target).siblings('dd'));
+        $.each(tabContents, function(tabKey, tabContent) {
+            if (tabKey == index) {
+             $(tabContent).removeClass('hidden')
+            }else{
+              $(tabContent).addClass('hidden')
+            }
+        })
+    })
+  });
+});

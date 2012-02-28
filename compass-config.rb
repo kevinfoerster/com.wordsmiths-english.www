b40f8b1e65d459cdab6d203b2fce5c21bf8a4049
Kevin Foerster <kevin.foerster@gmail.com>
2012-02-28 18:22:04 +0100

    optimized all pngs to reduce size enabled js minification enabled css minification removed folder created by rules even if they are not needed removed vogue.js

--- compass-config.rb
+++ compass-config.rb
@@ -2,8 +2,8 @@ require 'compass-h5bp'
 
 project_path = File.dirname(File.dirname(__FILE__))
 http_path = '/'
-# output_style = :compressed
-output_style = :expanded
+output_style = :compressed
+# output_style = :expanded
 sass_dir = 'content/css'
 css_dir = 'output/css'
 
